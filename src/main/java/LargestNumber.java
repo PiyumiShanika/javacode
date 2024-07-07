b0d5d494d80ac85e1d7d0bb121c2d53a9cf6efc3
PiyumiShanika <108194532+PiyumiShanika@users.noreply.github.com>
2024-07-07 11:02:55 +0530

    initial

---
+++ src/main/java/LargestNumber.java
@@ -0,0 +1,46 @@
+
+//import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+      //  Scanner  Largest = new  Scanner(System.in);
+
+     /*   System.out.print("Enter the first number: ");
+        int num1 =  Largest.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 =  Largest.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 =  Largest.nextInt();
+
+        int largest;
+
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+
+        System.out.println("The largest number is: " + largest);
+
+        Largest.close();
+       */
+
+
+        int x=40;
+        int y=x%2;
+
+        if(y==1){
+            System.out.println( "The  number is odd ");
+        }
+        else {
+            System.out.println( "The  number is even ");
+
+        }
+
+
+    }
+}
